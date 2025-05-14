d1211935d96caecfcd4d9ac23dacc36f9d9d901b
smodanesi <vsc33000@tier2-p-login-3.genius.hpc.kuleuven.be>
2025-05-14 17:01:33 +0200

    changed specification in the reader for SATlai rename

--- lis/surfacemodels/land/noahmp.3.6/pe/obspred/SATlai/NoahMP36_getpeobspred_SATlaiobs.F90
+++ lis/surfacemodels/land/noahmp.3.6/pe/obspred/SATlai/NoahMP36_getpeobspred_SATlaiobs.F90
@@ -8,14 +8,14 @@
 ! All Rights Reserved.
 !-------------------------END NOTICE -- DO NOT EDIT-----------------------
 !BOP
-! !ROUTINE: NoahMP36_getpeobspred_SYNTlaiobs
-!  \label{NoahMP36_getpeobspred_SYNTlaiobs}
+! !ROUTINE: NoahMP36_getpeobspred_SATlaiobs
+!  \label{NoahMP36_getpeobspred_SATlaiobs}
 !
 ! !REVISION HISTORY:
 ! 21 Jun 2023: Sara Modanesi; Initial Specification modified based on the SMAPsm dir
-!
+! 14 May 2025: Sara Modanesi; changed from SYNTlai to SATlai to allow confusion and calibrate with sat dat 
 ! !INTERFACE:
-subroutine NoahMP36_getpeobspred_SYNTlaiobs(Obj_Func)
+subroutine NoahMP36_getpeobspred_SATlaiobs(Obj_Func)
 ! !USES:
   use ESMF
   use LIS_coreMod, only : LIS_rc
@@ -41,7 +41,7 @@ subroutine NoahMP36_getpeobspred_SYNTlaiobs(Obj_Func)
 
   n = 1
 
-  call ESMF_StateGet(Obj_Func,"SYNT_lai",laiField,rc=status)
+  call ESMF_StateGet(Obj_Func,"SAT_lai",laiField,rc=status)
   call LIS_verify(status)
 
   call ESMF_FieldGet(laiField,localDE=0,farrayPtr=lai,rc=status)
@@ -51,7 +51,7 @@ subroutine NoahMP36_getpeobspred_SYNTlaiobs(Obj_Func)
      lai(t) = NoahMP36_struc(n)%noahmp36(t)%lai
   enddo
 
-end subroutine NoahMP36_getpeobspred_SYNTlaiobs
+end subroutine NoahMP36_getpeobspred_SATlaiobs
 
 
 
